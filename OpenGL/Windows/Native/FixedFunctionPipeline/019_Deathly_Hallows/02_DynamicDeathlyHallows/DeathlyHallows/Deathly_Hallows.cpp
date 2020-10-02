0f7ba613057ed2f0559da2041d72265cf9053dd1
Gauri04 <ranadegauri04@gmail.com>
2020-10-02 19:34:26 +0530

    Deathly Hallows

--- OpenGL/Windows/Native/FixedFunctionPipeline/019_Deathly_Hallows/02_DynamicDeathlyHallows/DeathlyHallows/Deathly_Hallows.cpp
+++ OpenGL/Windows/Native/FixedFunctionPipeline/019_Deathly_Hallows/02_DynamicDeathlyHallows/DeathlyHallows/Deathly_Hallows.cpp
@@ -22,14 +22,25 @@ HWND grghwnd = NULL;
 bool grgbActiveWindow = false;
 HDC grghdc = NULL;
 HGLRC grghrc = NULL;
-FILE *grgpFile = NULL;
+FILE* grgpFile = NULL;
+
+// project specific global variables declaration
+GLfloat grangleStone = 0.0f;
+GLfloat grangleWand = 0.0f;
+GLfloat grangleCloak = 0.0f;
+GLfloat grmoveStoneX = -3.0f;
+GLfloat grmoveStoneY = -2.0f;
+GLfloat grmoveCloakX = 3.0f;
+GLfloat grmoveCloakY = 2.0f;
+GLfloat grmoveWandY = 2.2f;
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
 {
 	// Function declaration
 	void Initialize(void);
 	void Display(void);
-	
+
 	// variables declaration
 	WNDCLASSEX wndclass;
 	HWND hwnd;
@@ -38,8 +49,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	int grDesktopWidth, grDesktopHeight;
 	int grWndXPos, grWndYPos;
 	bool grbDone = false;
-	
-	if(fopen_s(&grgpFile, "GRLog.txt", "w") != 0)
+
+	if (fopen_s(&grgpFile, "GRLog.txt", "w") != 0)
 	{
 		MessageBox(NULL, TEXT("Cannot open desired file"), TEXT("Error"), MB_OK | MB_ICONERROR);
 		exit(0);
@@ -48,7 +59,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	{
 		fprintf(grgpFile, "Log file created successfully. \n Program started successfully\n **** Logs ***** \n");
 	}
-	
+
 	wndclass.cbSize = sizeof(WNDCLASSEX);
 	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 	wndclass.cbClsExtra = 0;
@@ -61,53 +72,53 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	wndclass.lpszClassName = szAppName;
 	wndclass.lpszMenuName = NULL;
 	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(GRICON));
-	
+
 	RegisterClassEx(&wndclass);
-	
+
 	// Get width and height of desktop screen
 	grDesktopWidth = GetSystemMetrics(SM_CXSCREEN);
 	grDesktopHeight = GetSystemMetrics(SM_CYSCREEN);
-	
+
 	// Get center horizontal point
 	grDesktopWidth = grDesktopWidth / 2;
 	// Get center vertical point
 	grDesktopHeight = grDesktopHeight / 2;
-	
+
 	// X position = center horizontal coordinate of screen - center horizontal coordinate of window
 	grWndXPos = grDesktopWidth - 400;
-	
+
 	// X position = center horizontal coordinate of screen - center horizontal coordinate of window
 	grWndYPos = grDesktopHeight - 300;
-	
-	
+
+
 	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
-				szAppName,
-				TEXT("OpenGL"),
-				WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
-				grWndXPos,
-				grWndYPos,
-				WIN_WIDTH,
-				WIN_HEIGHT,
-				NULL,
-				NULL,
-				hInstance,
-				NULL);
-				
+		szAppName,
+		TEXT("OpenGL"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		grWndXPos,
+		grWndYPos,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
 	grghwnd = hwnd;
-	
+
 	Initialize();
-	
+
 	ShowWindow(hwnd, iCmdShow);
-	
+
 	SetForegroundWindow(hwnd);
 	SetFocus(hwnd);
-	
-	while(grbDone == false)
+
+	while (grbDone == false)
 	{
-		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-			if(msg.message == WM_QUIT)
-			grbDone = true;
+			if (msg.message == WM_QUIT)
+				grbDone = true;
 			else
 			{
 				TranslateMessage(&msg);
@@ -116,17 +127,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		}
 		else
 		{
-			if(grgbActiveWindow == true)
+			if (grgbActiveWindow == true)
 			{
 				//update function
-				
+
 				//display function
 				Display();
 			}
 		}
-		
+
 	}
-	
+
 	return((int)msg.wParam);
 }
 
@@ -136,70 +147,70 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	void ToggleFullScreen(void);
 	void Resize(int, int);
 	void Uninitialize(void);
-	
-	switch(iMsg)
+
+	switch (iMsg)
 	{
-		case WM_SETFOCUS : 
-			grgbActiveWindow = true;
-			break;
-			
-		case WM_KILLFOCUS :
-			grgbActiveWindow = false;
-			break;
-			
-		case WM_ERASEBKGND :
-			return(0);
-		
-			
-		case WM_SIZE :
-			Resize(LOWORD(lParam), HIWORD(lParam));
-			break;
-		
-		case WM_KEYDOWN : 
-			switch(wParam)
-			{
-				case VK_ESCAPE :
-					DestroyWindow(hwnd);
-					break;
-				
-				case 0x46 : 
-				case 0x66 :
-					ToggleFullScreen();
-					break;
-				
-				default : 
-					break;
-			}
-			break;
-			
-		case WM_CLOSE :
+	case WM_SETFOCUS:
+		grgbActiveWindow = true;
+		break;
+
+	case WM_KILLFOCUS:
+		grgbActiveWindow = false;
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+
+	case WM_SIZE:
+		Resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
 			DestroyWindow(hwnd);
 			break;
-		
-		case WM_DESTROY :
-			Uninitialize();
-			PostQuitMessage(0);
+
+		case 0x46:
+		case 0x66:
+			ToggleFullScreen();
 			break;
-		
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		Uninitialize();
+		PostQuitMessage(0);
+		break;
+
 	}
-	
+
 	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
 
 void ToggleFullScreen()
 {
 	MONITORINFO mi = { sizeof(MONITORINFO) };
-	
-	if(grgbFullScreen == false)
+
+	if (grgbFullScreen == false)
 	{
 		grdwStyle = GetWindowLong(grghwnd, GWL_STYLE);
-		if(grdwStyle & WS_OVERLAPPEDWINDOW)
+		if (grdwStyle & WS_OVERLAPPEDWINDOW)
 		{
-			if( GetWindowPlacement(grghwnd, &grgwpPrev) && GetMonitorInfo(MonitorFromWindow(grghwnd, MONITORINFOF_PRIMARY), &mi) )
+			if (GetWindowPlacement(grghwnd, &grgwpPrev) && GetMonitorInfo(MonitorFromWindow(grghwnd, MONITORINFOF_PRIMARY), &mi))
 			{
-				SetWindowLong( grghwnd, GWL_STYLE, (grdwStyle & ~ WS_OVERLAPPEDWINDOW) );
-				SetWindowPos( grghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top,
-					(mi.rcMonitor.right - mi.rcMonitor.left), (mi.rcMonitor.bottom - mi.rcMonitor.top), SWP_NOZORDER | SWP_FRAMECHANGED );
+				SetWindowLong(grghwnd, GWL_STYLE, (grdwStyle & ~WS_OVERLAPPEDWINDOW));
+				SetWindowPos(grghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top,
+					(mi.rcMonitor.right - mi.rcMonitor.left), (mi.rcMonitor.bottom - mi.rcMonitor.top), SWP_NOZORDER | SWP_FRAMECHANGED);
 			}
 		}
 		ShowCursor(false);
@@ -207,7 +218,7 @@ void ToggleFullScreen()
 	}
 	else
 	{
-		SetWindowLong( grghwnd, GWL_STYLE, (grdwStyle | WS_OVERLAPPEDWINDOW) );
+		SetWindowLong(grghwnd, GWL_STYLE, (grdwStyle | WS_OVERLAPPEDWINDOW));
 		SetWindowPlacement(grghwnd, &grgwpPrev);
 		SetWindowPos(grghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
 		ShowCursor(true);
@@ -219,14 +230,14 @@ void Initialize()
 {
 	// function declaration
 	void Resize(int, int);
-	
+
 	//variable declarations
 	PIXELFORMATDESCRIPTOR grpfd;
 	int griPixelFormatIndex;
-	
+
 	//code
 	grghdc = GetDC(grghwnd);
-	
+
 	ZeroMemory(&grpfd, sizeof(PIXELFORMATDESCRIPTOR));
 	grpfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
 	grpfd.nVersion = 1;
@@ -237,82 +248,81 @@ void Initialize()
 	grpfd.cGreenBits = 8;
 	grpfd.cBlueBits = 8;
 	grpfd.cAlphaBits = 8;
-	
+
 	griPixelFormatIndex = ChoosePixelFormat(grghdc, &grpfd);
-	if(griPixelFormatIndex == 0)
+	if (griPixelFormatIndex == 0)
 	{
 		fprintf(grgpFile, "ChoosePixelFormat() failed\n");
 		DestroyWindow(grghwnd);
 	}
-	
-	if(SetPixelFormat(grghdc, griPixelFormatIndex, &grpfd) == FALSE)
+
+	if (SetPixelFormat(grghdc, griPixelFormatIndex, &grpfd) == FALSE)
 	{
 		fprintf(grgpFile, "SetPixelFormat() failed\n");
 		DestroyWindow(grghwnd);
 	}
-	
+
 	grghrc = wglCreateContext(grghdc);
-	if(grghrc == NULL)
+	if (grghrc == NULL)
 	{
 		fprintf(grgpFile, "wglCreateContext() failed\n");
 		DestroyWindow(grghwnd);
 	}
-	
-	if(wglMakeCurrent(grghdc, grghrc) == FALSE)
+
+	if (wglMakeCurrent(grghdc, grghrc) == FALSE)
 	{
 		fprintf(grgpFile, "wglMakeCurrent() failed\n");
 		DestroyWindow(grghwnd);
 	}
-	
+
 	// set clearcolor
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	
+
 	// warm-up call to resize
 	Resize(WIN_WIDTH, WIN_HEIGHT);
 }
 
 void Resize(int width, int height)
 {
-	if(height == 0)
+	if (height == 0)
 		height = 1;
-	
+
 	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	
+
 	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
-	
+
 }
 
 void Display(void)
 {
+	// function declaration
+	void Update(void);
+
 	// local variable delaration
 	GLdouble gri, grs;
 	const GLdouble PI = 3.14159655359f;
 	GLdouble grlength1, grlength2, grlength3;
 	GLdouble grradius;
 	GLdouble grincenterX, grincenterY;
-	GLdouble grsqrt;
-	//GLdouble grbase, grheight, grarea;
+	GLdouble sqrt;
+	GLdouble grbase, grheight, grarea;
 
 	// code
 	////////////////////////////////////////////////////// STONE ///////////////////////////////////////////////////////////////
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
-	glTranslatef(0.0f, 0.0f, -6.0f);
+
+	glTranslatef(grmoveStoneX, grmoveStoneY, -6.0f);
+	glRotatef(grangleStone, 0.0f, 1.0f, 0.0f);
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_LINES); 
-		glVertex3f(0.0f, 1.0f, 0.0f);	// ax, ay
-		glVertex3f(-1.0f, -1.0f, 0.0f);	// bx, by
-
-		glVertex3f(-1.0f, -1.0f, 0.0f);	// bx, by
-		glVertex3f(1.0f, -1.0f, 0.0f);	// cx, cy	
-
-		glVertex3f(1.0f, -1.0f, 0.0f);	// cx, cy	
-		glVertex3f(0.0f, 1.0f, 0.0f);	// ax, ay
-	
+	glBegin(GL_LINE_LOOP);
+	glVertex3f(0.0f, 1.0f, 0.0f);	// ax, ay
+	glVertex3f(-1.0f, -1.0f, 0.0f);	// bx, by
+	glVertex3f(1.0f, -1.0f, 0.0f);	// cx, cy	
+	glVertex3f(0.0f, 1.0f, 0.0f);	// ax, ay
 	glEnd();
 
 	////////////////////////////////////////////////////// CLOAK ///////////////////////////////////////////////////////////////
@@ -325,85 +335,128 @@ void Display(void)
 	// distance between ax, ay and cx, cy : cx-ax, cy-ay (cx = 1.0, cy = -1.0) (ax = 0.0, ay = 1.0) VERTEX C
 	grlength2 = sqrtl(pow((1.0 - 0.0), 2) + pow((-1.0 - 1.0), 2));
 
-	//grlength1 = 2.0;
-	//grlength2 = sqrtl(5);
-	//grlength3 = sqrtl(5);
-
-	
 	// x = (ax*length1 + bx*length2 + cx*length3) / length1 + length2 + length3 
 	grincenterX = ((0.0f * grlength1) + (-1.0f * grlength2) + (1.0f * grlength3)) / (grlength1 + grlength2 + grlength3);
 	// y = (ay*length + by*length + cy*length) / 3*length
 	grincenterY = ((1.0f * grlength1) + (-1.0f * grlength2) + (-1.0f * grlength3)) / (grlength1 + grlength2 + grlength3);
-	
+
 	// s = (length1 + length2 + length3) / 2
 	grs = (grlength1 + grlength2 + grlength3) / 2;
 
-	//commented the formula for radius from wikipedia
+	/*	commented the formula for radius from wikipedia
 	// radius = (sqrt(s * (s - length1) * (s - length2) * (s - length3))) / s
-	grsqrt = grs * (grs - grlength1) * (grs - grlength2) * (grs - grlength3);
-	grsqrt = sqrtl(grsqrt);
-	grradius = grsqrt / grs;
-	
+	sqrt = grs * (grs - grlength1) * (grs - grlength2) * (grs - grlength3);
+	fprintf(grgpFile, "\n sqrt : %lf", sqrt);
+	sqrt = sqrtl(sqrt);
+	grradius = sqrt / grs;
+	*/
+
+	grbase = 2;
+	grheight = 2;
+	grarea = (2 * 2) / 2;
+	grradius = grarea / grs;
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glTranslated(0.0, 0.0, -6.0);
+	glTranslatef(grmoveCloakX, grmoveCloakY, -6.0f);
+	glRotatef(grangleCloak, 1.0f, 0.0f, 0.0f);
 	glColor3f(1.0f, 1.0f, 1.0f);
-	//fprintf(grgpFile, "\n incenter x = %lf incenter y = %lf, radius = %lf, length1 = %lf, length2 : %lf, length3 : %lf, s : %lf, sqrt : %lf", grincenterX, grincenterY, grradius, grlength1, grlength2, grlength3, grs, grsqrt);
+	//fprintf(grgpFile, "\n incenter x = %lf incenter y = %lf, radius = %lf, length1 = %lf, length2 : %lf, length3 : %lf, s : %lf, sqrt : %lf", grincenterX, grincenterY, grradius, grlength1, grlength2, grlength3, grs, sqrt);
 	glBegin(GL_POINTS);
-	for (gri = 0.0f; gri <= (2 * PI ); gri = gri + 0.005)
+	for (gri = 0.0f; gri <= (2 * PI); gri = gri + 0.005)
 	{
 		//glVertex2d(grincenterX, grincenterY);
 		glVertex2d((grincenterX + (grradius * cos(gri))), grincenterY + (grradius * sin(gri)));
-	}	
+	}
 	glEnd();
 
 	////////////////////////////////////////////////////// WAND ///////////////////////////////////////////////////////////////
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -6.0f);
+	glTranslated(0.0f, grmoveWandY, -6.0);
+	glRotatef(grangleWand, 0.0f, 0.0f, 1.0f);
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_LINES);
-		glVertex2f(0.0f, 1.0f);
-		glVertex2f(0.0f, -1.0f);
+	glVertex2f(0.0f, 1.0f);
+	glVertex2f(0.0f, -1.0f);
 	glEnd();
 
-	
+	Update();
 	SwapBuffers(grghdc);
+}
+
+void Update(void)
+{
+
+	if (grmoveStoneX <= 0.0f && grmoveStoneY <= 0.0f)
+	{
+
+		grmoveStoneX = grmoveStoneX + 0.000150f;
+		grmoveStoneY = grmoveStoneY + 0.00010f;
+		if (grangleStone >= 360.0f)
+		{
+			grangleStone = 0.0f;
+		}
+		grangleStone = grangleStone + 0.0090f;
+	}
+
+	if (grmoveCloakX >= 0.0f && grmoveCloakY >= 0.0f)
+	{
+		grmoveCloakX = grmoveCloakX - 0.000150f;
+		grmoveCloakY = grmoveCloakY - 0.00010f;
+		if (grangleCloak >= 360.0f)
+		{
+			grangleCloak = 0.0f;
+		}
+		grangleCloak = grangleCloak + 0.0180f;
+
+	}
+
+	if (grmoveWandY >= 0.0f)
+	{
+		grmoveWandY = grmoveWandY - 0.0001f;
+		if (grangleWand >= 360.0f)
+		{
+			grangleWand = 0.0f;
+		}
+		grangleWand = grangleWand + 0.04090f;
+	}
+
 }
 
 void Uninitialize(void)
 {
 	//code
-	if(grgbFullScreen == true)
+	if (grgbFullScreen == true)
 	{
 		grdwStyle = GetWindowLong(grghwnd, GWL_STYLE);
 		SetWindowLong(grghwnd, GWL_STYLE, (grdwStyle | WS_OVERLAPPEDWINDOW));
 		SetWindowPlacement(grghwnd, &grgwpPrev);
-		SetWindowPos(grghwnd, HWND_TOP, 0, 0, 0, 0, 
+		SetWindowPos(grghwnd, HWND_TOP, 0, 0, 0, 0,
 			SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
-		
+
 		ShowCursor(true);
-		
+
 	}
-	if(wglGetCurrentContext() == grghrc)
+	if (wglGetCurrentContext() == grghrc)
 	{
 		wglMakeCurrent(NULL, NULL);
 	}
-		
-	if(grghrc)
+
+	if (grghrc)
 	{
 		wglDeleteContext(grghrc);
 		grghrc = NULL;
 	}
-		
-	if(grghdc)
+
+	if (grghdc)
 	{
 		ReleaseDC(grghwnd, grghdc);
 		grghdc = NULL;
 	}
-		
-	if(grgpFile)
+
+	if (grgpFile)
 	{
 		fprintf(grgpFile, "\n **** End ****\nLog File closed successfully. \n Program terminated successfully");
 		fclose(grgpFile);
